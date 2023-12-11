c4d6ef223610f84bd19587742611d11b68ebfd04
GioSeguritech <134449728+GioSeguritech@users.noreply.github.com>
2023-12-11 10:20:17 -0600

    imprementacion de throughtput

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,97 +1,102 @@
-import React from 'react';
-import createPlotlyComponent from 'react-plotly.js/factory';
-import Plotly from 'plotly.js'
-import { useEffect } from 'react';
-import turf from 'turf';
-const Plot = createPlotlyComponent(Plotly);
+import React, { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
 
+const MapComponent = () => {
+  useEffect(() => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZ2lvcm9jaGEiLCJhIjoiY2xpZWJyZTR3MHpqcjNlcWRvaHR1em5sayJ9._SOrMWsc39Coa2dTHR072Q';
 
-const Map =({latitudes,longitudes,locations,handleMarkerClick,props})=>{
-  console.log(latitudes)
-// const Map =()=>{
- 
-    Plotly.setPlotConfig({
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/giorocha/clkirdmmx00ox01qm7js2bdas', // URL del estilo (elige uno de los estilos básicos de Mapbox o crea el tuyo propio con Mapbox Studio)
+      center: [-73.9709, 40.6712], // posición inicial [lng, lat]
+      zoom: 15.773 // nivel de zoom inicial
+    });
 
-        mapboxAccessToken: "pk.eyJ1IjoiZ2lvcm9jaGEiLCJhIjoiY2xpZWNhamQyMHp1azNlcGZ1YzhkOTVkayJ9.UCQO5iz1kFd9lMRxfUPSXA"
-      
-      })
-      
-      const data = latitudes
-      // const data = [
-      //   {
-      //     type: 'scattermapbox',
-      //     lat: [ '21.104694' ],
-      //     lon: [ '-101.695306' ],
-      //     text: [ "LEON-CUL01-CCTV-CAMBIUM_NETWORKS-PMP450i-SUSCRIPTOR-SUSCRIPTOR-GTO-VVL-LEON-001" ],
-      //     mode: 'markers',
-      //     marker: {
-      //       size:14,
-      //       color:'#b6ff4b',
-      //       symbol: 'square'
-           
-      //     },
-      //     hovertemplate:
-      //       "<b>%{customdata.id}...</b><br><br>",
-      //       hoverlabel: {
-      //         namelength: 10, // Establece el tamaño máximo del hovertemplate
-      //       },
-      //       customdata: [
-      //         {
-      //           nivel: 1,
-      //           name: 'hostidP.Host',
-      //           ip: 'goi.ip',
-      //           id: 'gio.hostidcon',
-      //         },
-      //       ],
-      //     // onHover={(data) => console.log('Marcador hover:', data)}
-          
-      //   },
-
-      // ];
-      const customMarkers = [
+    const geojson = {
+      type: 'FeatureCollection',
+      features: [
         {
-          name: 'custom-marker', // Nombre del símbolo
-          path: 'M10,0 A10,10 0 1,0 10,20 A10,10 0 1,0 10,0', // Definición del icono personalizado (puedes utilizar SVG o otras formas)
-        },
-      ];
-      const layout =  {
-        autosize: true,
-  hovermode:'closest',
-        mapbox: {
-          center: {
-            // lat: 20.5294858,
-            // lon: -100.7541315
-            lat: props.ubicacion.latitud,
-            lon: props.ubicacion.longitud,
-          },
-          domain: {
-            x: [0, 1],
-            y: [0, 1]
-          },
-          style: 'dark',
-          // zoom: props.ubicacion.zoom
-          zoom:10
-        },
-        margin: {
-          r: 0,
-          t: 0,
-          b: 0,
-          l: 0,
-          pad: 0
-        },
-        config: { responsive: true },
-        paper_bgcolor: '#191A1A',
-        plot_bgcolor: '#191A1A',
-        showlegend: false
-     };
-      return <Plot id='map' 
-            data={data} 
-            layout={layout} 
-            style={{ width: '100%',height:'100%', fontWeight: '900' }}  
-            onClick={handleMarkerClick}
-            
-            />;
-}
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            coordinates: [
+              [-73.97003, 40.67264],
+              [-73.97003, 40.67264],
 
-  
-export default Map
+              [-73.97228, 40.67008]
+            ],
+            type: 'LineString'
+          }
+        }
+      ]
+    };
+
+    map.on('load', () => {
+      map.addSource('line', {
+        type: 'geojson',
+        data: geojson
+      });
+
+      map.addLayer({
+        type: 'line',
+        source: 'line',
+        id: 'line-background',
+        paint: {
+          'line-color': 'yellow',
+          'line-width': 6,
+          'line-opacity': 0.4
+        }
+      });
+
+      map.addLayer({
+        type: 'line',
+        source: 'line',
+        id: 'line-dashed',
+        paint: {
+          'line-color': 'yellow',
+          'line-width': 6,
+          'line-dasharray': [0, 4, 3]
+        }
+      });
+
+      const dashArraySequence = [
+        [0, 4, 3],
+        [0.5, 4, 2.5],
+        [1, 4, 2],
+        [1.5, 4, 1.5],
+        [2, 4, 1],
+        [2.5, 4, 0.5],
+        [3, 4, 0],
+        [0, 0.5, 3, 3.5],
+        [0, 1, 3, 3],
+        [0, 1.5, 3, 2.5],
+        [0, 2, 3, 2],
+        [0, 2.5, 3, 1.5],
+        [0, 3, 3, 1],
+        [0, 3.5, 3, 0.5]
+        ];
+
+      let step = 0;
+
+      function animateDashArray(timestamp) {
+        const newStep = parseInt((timestamp / 50) % dashArraySequence.length);
+
+        if (newStep !== step) {
+          map.setPaintProperty('line-dashed', 'line-dasharray', dashArraySequence[step]);
+          step = newStep;
+        }
+
+        requestAnimationFrame(animateDashArray);
+      }
+
+      animateDashArray(0);
+    });
+
+    // Función de limpieza para eliminar el mapa cuando el componente se desmonta
+    return () => map.remove();
+  }, []); // Array de dependencias vacío para asegurar que el useEffect se ejecute solo una vez
+
+  return <div id="map" style={{ width: '100%', height: '400px' }}></div>; // Sustituye con el tamaño de mapa deseado
+};
+
+export default MapComponent;
